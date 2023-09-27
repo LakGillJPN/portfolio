20482aad2d99a001a0b79286d0f52ea3b4ecd7a1
LakGillJPN <lakgill88@gmail.com>
2023-09-27 11:08:30 +0900

    feat: add j-league fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <>
-    <h1> Lak Gill</h1>
-    </>
+    <div className="text-white font-bold">
+    <h1 className="text-6xl text-center"> Lak Gill Portfolio</h1>
+    <div className="grid-cols-1 text-4xl" >
+      <div className="font-family: JLEAGUE KICK Bold Condensed">J-League Predictor</div>
+      <div >GamerTalk</div>
+    </div>
+
+    </div>
   )
 }
