1091b99fdea52d77efb9eb1cc9c2d1fbf23ca7e3
LakGillJPN <lakgill88@gmail.com>
2023-09-27 14:31:54 +0900

    feat: add custom fonts for GamerTalk and J-League Predictor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,48 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Offside } from 'next/font/google'
+import localFont from 'next/font/local'
 
 const inter = Inter({ subsets: ['latin'] })
+export const offside = Offside({ subsets: ['latin'], weight:['400']})
 
 export const metadata: Metadata = {
   title: 'Lak Gill Portfolio',
 }
 
+export const jleague = localFont({
+  src: [
+    {
+      path: '../../public/fonts/j-league/JLEAGUEKICK-BoldCondensed.eot',
+      weight: 'bold',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/j-league/JLEAGUEKICK-BoldCondensed.ttf',
+      weight: 'bold',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/j-league/JLEAGUEKICK-BoldCondensed.woff',
+      weight: 'bold',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/j-league/JLEAGUEKICK-BoldCondensed.woff2',
+      weight: 'bold',
+      style: 'normal',
+    },
+  ],
+  variable: '--jleague'
+})
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${jleague.variable} font-sans`}>
+
       <body className={inter.className}>{children}</body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
 import Image from 'next/image'
+import { offside, jleague } from './layout'
 
 export default function Home() {
   return (
-    <div className="text-white font-bold">
-    <h1 className="text-6xl text-center"> Lak Gill Portfolio</h1>
-    <div className="grid-cols-1 text-4xl" >
-      <div className="font-family: JLEAGUE KICK Bold Condensed">J-League Predictor</div>
-      <div >GamerTalk</div>
-    </div>
+    <div className="text-white">
+    <h1 className="text-6xl text-center font-bold"> Lak Gill Portfolio</h1>
+    <div className="inline-grid grid-cols-2 gap-3 justify-evenly  text-4xl m-0" >
+
+      <div className="border-solid border-2 border-indigo-600"> 
+        <span className={jleague.className}> J-League Predictor</span>
+        <p>GitHub</p>
+      </div>
+
+      <div className="border-solid border-2 border-red-600">
+        <span className={offside.className} >GamerTalk</span>
+        <p>GitHub</p>
+      </div>
+
+   </div>
 
     </div>
   )
