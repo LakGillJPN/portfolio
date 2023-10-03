1244b104f317845599e05c3646c9eed37da8db10
LakGillJPN <lakgill88@gmail.com>
2023-10-03 11:17:20 +0900

    feat: add more images of J-League Predictor and GamerTalk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,14 +42,17 @@ export default function Home() {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 text-4xl">
         <div className="text-5xl border-solid border-2 border-red-600 text-center rounded-xl p-4 flex flex-col items-center">
-          <span className={jleague.className}>J-League Predictor</span>
-          <p className="text-sm p-5">
+          <p className="border-solid border-2 border-blue-600 h-20"><span className={jleague.className}>J-League Predictor</span></p>
+          <p className="border-solid border-2 border-yellow-600 text-sm p-5 flex-grow flex items-center justify-center">
             A game where users predict the results of upcoming J-League fixtures
             and earn points based on their accuracy.
           </p>
+          <p className="border-solid border-2 border-yellow-600 text-sm p-5 flex-grow flex items-center justify-center">
+          <a className="text-blue-500 underline" href="https://j-league-predictor.vercel.app/"> 
+             https://j-league-predictor.vercel.app/</a></p>
           <div className="flex items-center justify-center">
             <Image
-              src="/images/j-league-screenshot.png"
+              src="/images/J-League-Fullscreen.png"
               width={600}
               height={600}
               alt="GamerTalk screenshot"
@@ -69,20 +72,24 @@ export default function Home() {
         </div>
 
         <div className="text-5xl border-solid border-2 border-purple-600 text-center rounded-xl p-4 flex flex-col items-center">
-          <span className={offside.className}>GamerTalk</span>
-          
-            <p className="text-sm p-5">
+          <p className="border-solid border-2 border-blue-600 h-20"><span className={offside.className}>GamerTalk</span> </p>
+            <p className="border-solid border-2 border-yellow-600 text-sm p-5 flex-grow flex items-center justify-center">
               An application that allows gamers to connect with each other with
               the goal of building connections.
             </p>
+            <p className="border-solid border-2 border-yellow-600 text-sm p-5 flex-grow flex items-center justify-center ">
+              <a className="text-blue-500 underline" href="https://gamertalk.onrender.com/">
+              https://gamertalk.onrender.com/</a></p>
+
             <div className="flex items-center justify-center">
               <Image
-                src="/images/gamertalk-screenshot.png"
+                src="/images/Gamertalk-fullscreen.png"
                 width={600}
                 height={600}
                 alt="GamerTalk screenshot"
               />
             </div>
+
             <a href="https://github.com/GamerTalk">
             <div className="border-solid flex justify-center items-center rounded-xl mx-auto  m-10 p-3 hover:bg-slate-700 bg-slate-600">
               <Image
