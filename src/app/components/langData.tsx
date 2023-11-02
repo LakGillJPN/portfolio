1fa2a5b652f194f6040fc3cb1f5dc5994d56830e
LakGillJPN <lakgill88@gmail.com>
2023-11-02 09:57:59 +0900

    feat: add langData.tsx

---
+++ src/app/components/langData.tsx
@@ -0,0 +1,12 @@
+const languages = {
+  english: {
+    header: 'Lak Gill Portfolio',
+    button: '日本語',
+  },
+  japanese: {
+    header: 'ラク ギル ポートフォリオ',
+    button: 'English',
+  },
+};
+
+export default languages;
