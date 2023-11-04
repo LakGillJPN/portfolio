39c7883591376e68981accf05ad73e52f48b8777
LakGillJPN <lakgill88@gmail.com>
2023-11-04 11:51:18 +0900

    fix: remove case changing in git

---
+++ src/app/components/ProjectFramework.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Image from 'next/image';
+import GitHubLogo from '../logos/misc/GitHubLogo';
+
+interface CommonProps {
+  title: string;
+  font: string;
+  text: string;
+  url: string;
+  imageSrc: string;
+  logos: React.ReactNode[];
+  githubUrl: string;
+  borderColor: string;
+}
+
+const ProjectFramework: React.FC<CommonProps> = ({
+  title,
+  font,
+  text,
+  url,
+  imageSrc,
+  logos,
+  githubUrl,
+  borderColor,
+}) => {
+  return (
+    <div className={`text-5xl border-solid border-2 border-${borderColor}-600 text-center rounded-xl p-4 flex flex-col items-center`}>
+      <p className="h-20">
+        <span className={font}>{title}</span>
+      </p>
+      <p className="text-sm p-5 flex-grow flex items-center justify-center">{text}</p>
+      <p className="text-lg p-5 flex-grow flex items-center justify-center">
+        <a className="text-blue-500 underline" href={url}>
+          {url}
+        </a>
+      </p>
+      <div className="flex items-center justify-center">
+        <Image src={imageSrc} width={600} height={600} alt={`${title} screenshot`} />
+      </div>
+      <div className="flex flex-wrap pt-5">{logos}</div>
+      <a href={githubUrl}>
+        <div className="border-2 border-solid flex justify-center items-center rounded-xl mx-auto m-10 p-3 hover:bg-slate-700 bg-slate-600">
+          <GitHubLogo />
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default ProjectFramework
--- src/app/components/projects/GamerTalk.tsx
+++ src/app/components/projects/GamerTalk.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Image from 'next/image'
 import { offside } from "../../layout";
 import NextJSLogo from '../../logos/frontend/NextJSLogo';
 import TypeScriptLogo from '../../logos/languages/TypeScriptLogo';
@@ -8,13 +7,11 @@ import CypressLogo from '../../logos/testing/CypressLogo';
 import FirebaseLogo from '../../logos/misc/FirebaseLogo';
 import PostgresLogo from '../../logos/datebase/PostgresLogo';
 import DjnagoLogo from '../../logos/backend/DjnagoLogo';
-import GitHubLogo from '../../logos/misc/GitHubLogo';
 import ProjectFramework from '../ProjectFramework';
 
 interface GamerTalkProps {
   text: string;
 }
-
 
 const GamerTalk: React.FC<GamerTalkProps> = ({ text }) =>  {
   const logos = [  <NextJSLogo/>,
@@ -24,6 +21,7 @@ const GamerTalk: React.FC<GamerTalkProps> = ({ text }) =>  {
   <CypressLogo/>,
   <FirebaseLogo/>,
   <PostgresLogo/>]
+
   return (
     <ProjectFramework 
     title="GamerTalk"
@@ -35,43 +33,6 @@ const GamerTalk: React.FC<GamerTalkProps> = ({ text }) =>  {
     githubUrl="https://github.com/GamerTalk"
     borderColor="purple"  
     />
-      
-
-
-  //   <div className="text-5xl border-solid border-2 border-purple-600 text-center rounded-xl p-4 flex flex-col items-center">
-  //   <p className=" h-20"><span className={offside.className}>GamerTalk</span> </p>
-  //     <p className="text-sm p-5 flex-grow flex items-center justify-center">
-  //       {text}
-  //     </p>
-  //     <p className="text-lg p-5 flex-grow flex items-center justify-center ">
-  //       <a className="text-blue-500 underline" href="https://gamertalk.onrender.com/">
-  //       https://gamertalk.onrender.com/</a></p>
-
-  //     <div className="flex items-center justify-center">
-  //       <Image
-  //         src="/images/Gamertalk-fullscreen.png"
-  //         width={600}
-  //         height={600}
-  //         alt="GamerTalk screenshot"
-  //       />
-  //     </div>
-
-  //     <div className="flex flex-wrap pt-5">
-  //       <NextJSLogo/>
-  //       <TypeScriptLogo/>
-  //       <PythonLogo/>
-  //       <DjnagoLogo/>
-  //       <CypressLogo/>
-  //       <FirebaseLogo/>
-  //       <PostgresLogo/>
-  //     </div>
-
-  //     <a href="https://github.com/GamerTalk">
-  //     <div className="border-2 border-solid flex justify-center items-center rounded-xl mx-auto  m-10 p-3 hover:bg-slate-700 bg-slate-600">
-  //       <GitHubLogo/>
-  //     </div>
-  //   </a>
-  // </div>
   )
 }
 
