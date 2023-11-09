44e3163dccdaa30c49140f0eba7aea808671c938
LakGillJPN <lakgill88@gmail.com>
2023-11-09 23:01:47 +0900

    feat: add target to a tags in Header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import Image from 'next/image';
 
-interface FooterProps {
+interface HeaderProps {
   text: string;
 }
 
-const Header: React.FC<FooterProps> = ({text} ) => {
+const Header: React.FC<HeaderProps> = ({text} ) => {
   return (
     <>
       <div className="text-2xl font-bold p-5 bg-grey-800 flex" > 
-        <a href="https://www.linkedin.com/in/lak-gill/">
+        <a href="https://www.linkedin.com/in/lak-gill/" target="_blank">
            <Image 
             src="/images/li-logo-white.png"
             width={50}
@@ -17,7 +17,7 @@ const Header: React.FC<FooterProps> = ({text} ) => {
             alt="link to LinkedIn"
             />
         </a>
-        <a className="pl-4" href="https://www.twitter.com/LakGill88">
+        <a className="pl-4" href="https://www.twitter.com/LakGill88" target="_blank">
           <Image 
            src="/images/x-logo-white.png"
            width={50}
@@ -25,7 +25,7 @@ const Header: React.FC<FooterProps> = ({text} ) => {
            alt="link to X"
           />
         </a>
-        <a className="pl-4" href="https://www.twitter.com/LakGill88">
+        <a className="pl-4" href="https://github.com/LakGillJPN" target="_blank">
           <Image 
            src="/images/github-mark-white.png"
            width={50}
