891795c7778c00350c3956d86d2c78ef63aa679b
LakGillJPN <lakgill88@gmail.com>
2023-11-17 06:24:30 +0900

    move button into Header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Image from 'next/image';
+import languages from './langData';
 
 interface HeaderProps {
   text: string;
+  changeFunc: React.MouseEventHandler<HTMLButtonElement>;
+  lang: string;
 }
 
-const Header: React.FC<HeaderProps> = ({text} ) => {
+const Header: React.FC<HeaderProps> = ({text, changeFunc, lang} ) => {
   return (
     <>
+       <button
+        onClick={changeFunc}
+        className="text-2xl font-bold p-3 border-2 rounded-xl absolute top-0 right-0 m-4 hover:bg-slate-700 bg-slate-600">
+        {languages[lang].button}
+      </button>
+
       <div className="text-2xl font-bold p-5 bg-grey-800 flex" > 
         <a href="https://www.linkedin.com/in/lak-gill/" target="_blank">
            <Image 
