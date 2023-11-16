948677b52855daac5472fa72c7931fe6b8e71f74
LakGillJPN <lakgill88@gmail.com>
2023-11-16 15:09:18 +0900

    feat: add AboutMe.tsx

---
+++ src/app/aboutme/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header'
+
+export default function AboutMe() {
+  return (
+    <div>
+      <Header text=""/>
+      <h1>About Me</h1>
+      
+    </div>
+  )
+}
