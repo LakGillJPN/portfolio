bce6149ab42db34f6b715debccd9b813c0a6af24
LakGillJPN <lakgill88@gmail.com>
2023-11-29 09:42:26 +0900

    refactor: move code to change language into Header

--- src/app/aboutme/page.tsx
+++ src/app/aboutme/page.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react'
 import Header from '../components/Header'
 
 export default function AboutMe() {
+  const [lang, setLang] = useState<string>("English");
+
+  const changeLang = () => {
+    return lang === "English" ? setLang("Japanese") : setLang("English");
+  };
+
   return (
     <div>
-      <Header text=""/>
+      <Header text="" changeFunc={changeLang} lang={lang}/>
       <h1>About Me</h1>
       
     </div>
