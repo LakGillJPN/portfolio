4478a7002ef33d31f0fdaf622f60735f443f8ec2
LakGillJPN <lakgill88@gmail.com>
2023-12-30 21:37:03 +0900

    refactor: change Menu text to use FontAwesome icon

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Image from 'next/image';
 import languages from './langData';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 interface HeaderProps {
   text: string;
@@ -21,9 +23,9 @@ const Header: React.FC<HeaderProps> = ({text, changeFunc, lang, toggleMenu} ) =>
       <div className="text-2xl font-bold p-5 bg-grey-800 flex" > 
       <button
         onClick={toggleMenu}
-        className="text-white text-2xl font-bold p-3 border-2 rounded-xl absolute top-0 left-0 m-4 hover:bg-slate-700 bg-slate-600"
+        className="text-white text-2xl font-bold p-3 absolute top-0 left-0 m-4"
       >
-        Menu
+      <FontAwesomeIcon icon={faBars} />
       </button> 
       
     </div>
