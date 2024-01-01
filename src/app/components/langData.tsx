e68c45490d46ad7de082e1ddb4c6a3e713914ff9
LakGillJPN <lakgill88@gmail.com>
2024-01-01 17:22:22 +0900

    feat: add back to top message in langData

--- src/app/components/langData.tsx
+++ src/app/components/langData.tsx
@@ -1,5 +1,6 @@
 type Language = {
   button: string;
+  top: string;
   JLeague: string;
   GamerTalk: string;
   projects: string;
@@ -13,9 +14,10 @@ type Language = {
 const languages: Record<string, Language> = {
   English: {
     button: '日本語',
+    top: "Back To Top",
+    projects: "Projects",
     JLeague: "A game where users predict the results of upcoming J-League fixtures and earn points based on their accuracy." ,
     GamerTalk: "An application that allows gamers to connect with each other with the goal of building connections.",
-    projects: "Projects",
     freelance: "Contributions",
     mamoruDate: "Oct 2023 - Present",
     mamoru: "mamoru is a sustainable living app that helps you discover and save on sustainable products, shops, cafes, restaurants, and more.",
@@ -23,9 +25,10 @@ const languages: Record<string, Language> = {
   },
   Japanese: {
     button: 'English',
+    top: "トップへ戻る",
+    projects: "プロジェクト",
     JLeague: "Jリーグの勝敗を予想し、ポイントを獲得するゲーム。",
     GamerTalk: "ゲーマーをつないで語学を学ぶソーシャル・ネットワーキング・アプリ。",
-    projects: "プロジェクト",
     freelance: "フリーランス",
     mamoruDate: "2023年10月～現在",
     mamoru: "mamoru（まもる）は、サステナブルな商品、ショップ、カフェ、レストランなどを発見し、お得に利用することができる サステナブルライフアプリです。",
